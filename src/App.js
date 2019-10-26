9372833c90c7360071461658982b5789f4bece42
erneledesma <erneledesma@gmail.com>
2019-10-26 18:40:18 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Productos from './components/Productos'
+import EditarProducto from './components/EditarProducto'
+import AgregarProducto from './components/AgregarProducto'
+import Producto from './components/Producto'
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Header/>
+      <main className="container mt-5">
+      <Switch>
+        <Route exact path="/nuevo-producto" component={AgregarProducto}></Route>
+        <Route exact path="/productos" component={Productos}></Route>
+        <Route exact path="/productos/:id" component={Producto}></Route>
+        <Route exact path="productos/editar/:id" component={EditarProducto}></Route>
+      </Switch>
+      </main>
+      <p className="mt-4 p2 text-center">Todos los derechos reservados</p>
+    </Router>
   );
 }
 
---
+++ src/components/Productos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Producto() {
+     
+    return (
+        <h1 className="text-center"> Productos</h1>
+    )
+}
+
+export default Producto;
